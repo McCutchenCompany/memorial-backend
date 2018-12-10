0c2f4a46ed5dd40a49451585feeb3b85b823ebc6
Mitch <mitch@linuxacademy.com>
2018-12-10 16:20:45 -0600

    remove ssh-add

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
+# echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
 
 #Get servers list
 string=$STAGING_DEPLOY_SERVER
