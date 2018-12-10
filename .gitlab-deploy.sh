e07468bcc8762a1ffabd1032e1a40b35034290be
Mitch <mitch@linuxacademy.com>
2018-12-10 15:33:39 -0600

    use sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,8 +6,5 @@ eval $(ssh-agent -s)
 
 #Get servers list
 string=$STAGING_DEPLOY_SERVER
-echo string
-array=[${string}]
 #Iterate servers for deploy and pull last commit
-  echo "Deploy project on server $STAGING_DEPLOY_SERVER"    
-  ssh -i $SSH_PRIVATE_KEY ec2-user@$STAGING_DEPLOY_SERVER "docker container ls"
+ssh -i $SSH_PRIVATE_KEY ec2-user@$STAGING_DEPLOY_SERVER deploy/deployAndRestart.sh
