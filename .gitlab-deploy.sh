42e313f69dc822bde94f565c23a09572c584dd25
Mitch <mitch@linuxacademy.com>
2018-12-10 16:33:02 -0600

    try sshpass

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-eval $(ssh-agent -s)
+# eval $(ssh-agent -s)
 # echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
-cat memorial-backend.pem
+# cat memorial-backend.pem
 #Get servers list
 string=$STAGING_DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
-ssh -i memorial-backend.pem ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
+# ssh -i memorial-backend.pem ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
+sshpass -p "" ssh -o StrictHostKeyChecking=no ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh"
