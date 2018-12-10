4fceb807264dce6fe97e429ec42e55260b3df26f
Mitch <mitch@linuxacademy.com>
2018-12-10 16:01:52 -0600

    create file

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,9 +2,11 @@
 set -e
 
 eval $(ssh-agent -s)
+$SSH_PRIVATE_KEY >> memorial-backend.pem
+chmod 400 memorial-backend.pem
 # echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
 
 #Get servers list
 string=$STAGING_DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
-ssh $SSH_PRIVATE_KEY ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
+ssh -i memorial-backend.pem ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
