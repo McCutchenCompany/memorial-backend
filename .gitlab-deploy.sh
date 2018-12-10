241186a693e11e0633fa323daf8be2a2878e0728
Mitch <mitch@linuxacademy.com>
2018-12-10 16:27:24 -0600

    print file

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 eval $(ssh-agent -s)
 # echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
-ls -l
+cat memorial-backend.pem
 #Get servers list
 string=$STAGING_DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
