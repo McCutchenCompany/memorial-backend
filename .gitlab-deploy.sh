d288926e95a6382dd70fa0d7f11af4a5d8c39a43
Mitch <mitch@linuxacademy.com>
2018-12-10 16:10:37 -0600

    USES PRIVATE key var

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -8,4 +8,4 @@ eval $(ssh-agent -s)
 string=$STAGING_DEPLOY_SERVER
 ls
 #Iterate servers for deploy and pull last commit
-ssh -i memorial-backend.pem ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
+ssh -i $SSH_PRIVATE_KEY ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh" 
