18be10d3d830724084ec80143d7c9329ab536928
Mitch <mitch@linuxacademy.com>
2018-12-10 14:46:38 -0600

    new config

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+set -e
 
-touch ~/.ssh/config
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '/r' | ssh-add - > /dev/null
 
+./disableHostKeyChecking.sh
+
 #Get servers list
-set -f
-if [ "$1" == "staging"]
-then
 string=$STAGING_DEPLOY_SERVER
 echo string
-fi
 array=[${string}]
 #Iterate servers for deploy and pull last commit
   echo "Deploy project on server $STAGING_DEPLOY_SERVER"    
-  ssh ec2-user@$STAGING_DEPLOY_SERVER "cd memorial-backend && git pull origin staging"
+  ssh ec2-user@$STAGING_DEPLOY_SERVER 'bash' < "cd memorial-backend && git pull origin staging"
