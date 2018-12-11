096bbbdad012d3c3be2121b845c0de2802c10239
Mitch <mitch@linuxacademy.com>
2018-12-11 13:45:10 -0600

    Adds migration for desc on memorials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_190856) do
+ActiveRecord::Schema.define(version: 2018_12_11_194356) do
 
   create_table "locations", primary_key: "uuid", id: :binary, limit: 36, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "memorial_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_190856) do
     t.datetime "death_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "timelines", primary_key: "uuid", id: :binary, limit: 36, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
