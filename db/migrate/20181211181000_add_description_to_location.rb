0e33cde38a0a29c3c3d9a06b99d1dbdd190a5e36
Mitch <mitch@linuxacademy.com>
2018-12-11 14:43:42 -0600

    change descriptions to text

--- db/migrate/20181211181000_add_description_to_location.rb
+++ db/migrate/20181211181000_add_description_to_location.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToLocation < ActiveRecord::Migration[5.2]
   def change
-    add_column :locations, :description, :string
+    add_column :locations, :description, :text
   end
 end
