9fc40a80baa4cb34e9c7e6e8095ea11b51ec2387
Mitch <mitch@linuxacademy.com>
2018-12-30 20:38:24 -0600

    return memorials with user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: {user: @user, memorials: @user.memorial}
     else
       render json: @user.errors, status: :unprocessable_entity
     end
