8f6351687631233404b1aab943a3c3cbf27e9900
Mitch <mitch@linuxacademy.com>
2018-12-31 21:36:40 -0600

    Removes where

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,7 +4,7 @@ class Location < ApplicationRecord
   belongs_to :memorial
 
   def self.find_in_range(range)
-    @locations = Location.where.where(:latitude => range[:bottom].to_f..range[:top].to_f).where(:longitude => range[:left].to_f..range[:right].to_f)
+    @locations = Location.where(:latitude => range[:bottom].to_f..range[:top].to_f).where(:longitude => range[:left].to_f..range[:right].to_f)
     location_array = []
     @locations.each { |location| 
       if Memory.find_by(uuid: location[:memorial_id])[:published]
