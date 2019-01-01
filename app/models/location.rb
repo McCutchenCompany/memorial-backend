73b56f65fb6e5eda1ac12fa6f7fe535b90af87c0
Mitch <mitch@linuxacademy.com>
2018-12-31 21:32:58 -0600

    adds published to range

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,13 @@ class Location < ApplicationRecord
   belongs_to :memorial
 
   def self.find_in_range(range)
-    Location.where(published: true).where(:latitude => range[:bottom].to_f..range[:top].to_f).where(:longitude => range[:left].to_f..range[:right].to_f)
+    @locations = Location.where.where(:latitude => range[:bottom].to_f..range[:top].to_f).where(:longitude => range[:left].to_f..range[:right].to_f)
+    location_array = []
+    @locations.each { |location| 
+      if Memory.find_by(uuid: location[:memorial_id])[:published]
+        location_array.push(location)
+      end
+    }
+    location_array
   end
 end
