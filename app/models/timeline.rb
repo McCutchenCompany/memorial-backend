587311eb0509cde2d5632557cb9324c7edc11695
Mitch <mitch@linuxacademy.com>
2019-01-04 09:12:47 -0600

    update params for timeline items

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -6,7 +6,7 @@ class Timeline < ApplicationRecord
   def self.bulk_update(timelines, current_memorial)
     timelines.each do |t|
       @timeline = Timeline.where({memorial_id: current_memorial, uuid: t[:uuid]})[0]
-      unless @timeline.update(t.permit(:date, :date_format, :description, :event, :title))
+      unless @timeline.update(t.permit(:date, :date_format, :description, :event, :title, :asset_type, :asset_link))
         render json: @timeline.errors, status: :unprocessable_entity
       end
     end
