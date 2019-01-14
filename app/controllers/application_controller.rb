ff1dc23e22528a0b45dd13910865b267b40bff16
Mitch <mitch@linuxacademy.com>
2019-01-14 14:52:04 -0600

    return specific type of address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::API
   end
 
   def reverse_geocode
-    if @address = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + params[:latitude].to_s + ',' + params[:longitude].to_s + '&key=' + ENV['GOOGLE_KEY'],
+    if @address = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?result_type=locality&latlng=' + params[:latitude].to_s + ',' + params[:longitude].to_s + '&key=' + ENV['GOOGLE_KEY'],
       headers: {'Content-Type': 'application/json'})
-      description = @address["results"][5]["formatted_address"]
+      return @address["results"][0]["formatted_address"]
     else 
       render json: {error: "Trouble getting the formatted address"}, status: :unprocessable_entity
     end
