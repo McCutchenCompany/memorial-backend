f039e0a88a091db513c880ee6bbc1e11d17ddca4
Mitch <mitch@linuxacademy.com>
2019-01-15 11:42:31 -0600

    Add fallback result types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::API
   end
 
   def reverse_geocode
-    if @address = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?result_type=locality&latlng=' + params[:latitude].to_s + ',' + params[:longitude].to_s + '&key=' + ENV['GOOGLE_KEY'],
+    if @address = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?result_type=locality|administrative_area_level_2|administrative_area_level_1|country&latlng=' + params[:latitude].to_s + ',' + params[:longitude].to_s + '&key=' + ENV['GOOGLE_KEY'],
       headers: {'Content-Type': 'application/json'})
-      return @address["results"][0]["formatted_address"]
+      if @address["results"][0]["formatted_address"]
+        @address["results"][0]["formatted_address"]
+      else
+        render json: {error: "Trouble getting the formatted address"}, status: :unprocessable_entity
+      end
     else 
       render json: {error: "Trouble getting the formatted address"}, status: :unprocessable_entity
     end
