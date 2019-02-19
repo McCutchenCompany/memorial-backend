ea3252fd8adb0cd0f90962d266b590a3a87f9458
Mitch <mitch@linuxacademy.com>
2019-02-18 20:22:31 -0600

    Prod deploy scripts

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
+if [ "$1" == "staging" ]
+then
+  sshpass ssh -o StrictHostKeyChecking=no ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh"
 
-sshpass ssh -o StrictHostKeyChecking=no ec2-user@$STAGING_DEPLOY_SERVER "deploy/deployAndRestart.sh"
+elif [ "$1" == "master" ]
+then
+  sshpass ssh -o StrictHostKeyChecking=no ec2-user@$PROD_DEPLOY_SERVER "deploy/deployAndRestart.sh"
+fi
