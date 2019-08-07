ff36a94cdcb8826553ba702819a9040e759859b2
Mitch <mitch@linuxacademy.com>
2019-08-06 22:30:57 -0500

    Change to memorials

--- app/controllers/memorials_controller.rb
+++ app/controllers/memorials_controller.rb
@@ -343,7 +343,7 @@ class MemorialsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_memorial
-      if @memorial = @user.memorial.where(uuid: params[:id])[0]
+      if @memorial = @user.memorials.where(uuid: params[:id])[0]
         return
       else
         memorial = Memorial.find(params[:id])
